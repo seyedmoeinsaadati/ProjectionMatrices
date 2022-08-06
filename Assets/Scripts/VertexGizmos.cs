88f151eccb0dc4c4fc88c6a34ba189d751a6ff7d
Seyed Moein Saadati <saadati.moin@gmail.com>
2022-08-06 13:10:42 +0430

    Add VertexGizmos.cs

---
+++ Assets/Scripts/VertexGizmos.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class VertexGizmos : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    private Mesh mesh;
+
+    private void OnDrawGizmos()
+    {
+        if (mesh != null)
+        {
+            Gizmos.color = Color.black;
+            for (int i = 0; i < mesh.vertexCount; i++)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(mesh.vertices[i]), .05f);
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+    }
+}
