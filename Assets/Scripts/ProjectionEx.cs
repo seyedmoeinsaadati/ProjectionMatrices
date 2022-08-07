b4a9e45a88f051d543d30e80058d79383dee1ba0
Seyed Moein Saadati <saadati.moin@gmail.com>
2022-08-07 14:35:37 +0430

    Add wobble effect as an extension mehtod

---
+++ Assets/Scripts/ProjectionEx.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class ProjectionEx
+{
+    public static Matrix4x4 Wobble(this Matrix4x4 self, float speed)
+    {
+        self.m01 += Mathf.Cos(Time.time * 1.2F) * 0.1F * speed;
+        self.m10 += Mathf.Sin(Time.time * 1.5F) * 0.1F * speed;
+        return self;
+    }
+}
